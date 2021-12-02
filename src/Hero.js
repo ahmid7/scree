04862eac1bf0444bc49a4654d5a1258f245e0e24
ahmid <ahmidsanni@gmail.com>
2021-12-02 03:52:23 -0800

    fixed hamburger

--- src/Hero.js
+++ src/Hero.js
@@ -1,12 +1,9 @@
 import { useState } from "react";
 import { RiSearch2Line,RiShoppingCartLine } from "react-icons/ri";
-import { CgMenuRightAlt } from "react-icons/cg";
+import Hamburger from "hamburger-react";
 
 const Hero = () => {
-    const [navShown,setNavshown] = useState(false);
-    const handleNavClick = () =>{
-        setNavshown(!setNavshown)
-    }
+    const [isOpen, setOpen] = useState(false);
     return(
         <div className="hero-bg">
             <div className="hero-header">
@@ -14,7 +11,7 @@ const Hero = () => {
                     <img src="images/logo.png" alt="logo"/>
                     <h2 className="logo-text">Furnie</h2>
                 </div>
-                <ul className= {`${navShown ? 'block' : 'hidden'}`}>
+                <ul className= {`${isOpen ? 'block' : 'hidden'}`}>
                     <li><a href="#home">home</a></li>
                     <li><a href="#product">Product</a></li>
                     <li><a href="#contact">Contact Us</a></li>
@@ -27,9 +24,9 @@ const Hero = () => {
                         <RiShoppingCartLine className="cart-icon"/>
                         <p>0</p>
                     </div>
-                    <button class="hamburger" >
-                        <CgMenuRightAlt onClick= {handleNavClick}/>
-                    </button>
+                    <div class="hamburger-container" >
+                        <Hamburger toggled={isOpen} toggle={setOpen} distance ="sm" size={18}/>
+                    </div>
                 </div>
             </div>
             <section className="hero-content">
